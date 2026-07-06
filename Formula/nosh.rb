6f9a14e445902dac9e545c889ff86dc13e32581b
Guilherme <reach.guilherme@gmail.com>
2026-07-06 17:07:06 +0200

    bump to v0.2.1

--- Formula/nosh.rb
+++ Formula/nosh.rb
@@ -1,15 +1,15 @@
 class Nosh < Formula
   desc "TUI todo and notes manager"
   homepage "https://github.com/gosantos/nosh"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gosantos/nosh/releases/download/v0.2.0/nosh-aarch64-apple-darwin.tar.gz"
-      sha256 "472f68e6e1ebbeed1f07d91dcf213ec056186150d15a6d0cf413c876e27d5a38"
+      url "https://github.com/gosantos/nosh/releases/download/v0.2.1/nosh-aarch64-apple-darwin.tar.gz"
+      sha256 "acdd3e4437fa4905430f638d0345c130c23d527fb3318f93ccb880513c704559"
     else
-      url "https://github.com/gosantos/nosh/releases/download/v0.2.0/nosh-x86_64-apple-darwin.tar.gz"
-      sha256 "d20fdfb919ecf1503697a6344e22e56c0e59db336e666af99a5571e055582d2f"
+      url "https://github.com/gosantos/nosh/releases/download/v0.2.1/nosh-x86_64-apple-darwin.tar.gz"
+      sha256 "440a7cb6cfee99da894cea9990bf82e9c26b42b2bd3b9436b34524020f0d87e1"
     end
   end
 
