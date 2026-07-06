146d16a303a7d9357609e0c82f7d0fd0194af9f2
Guilherme <reach.guilherme@gmail.com>
2026-07-06 16:03:55 +0200

    update checksums for v0.1.0

--- Formula/nosh.rb
+++ Formula/nosh.rb
@@ -6,10 +6,10 @@ class Nosh < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gosantos/nosh/releases/download/v0.1.0/nosh-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      sha256 "7b3963d2e916027893fb2cb2879bf5eb071f7cf0741453ca106321d68f4c0e85"
     else
       url "https://github.com/gosantos/nosh/releases/download/v0.1.0/nosh-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_INTEL_SHA256"
+      sha256 "1e66ac74f216481a88faf0e0e6dacb1f4e4055eb3d461f949ebc7a7c6ad8292e"
     end
   end
 
