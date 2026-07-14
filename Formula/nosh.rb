fc636a4b8427d1514fec5e148edbe6eec09cf759
Guilherme <reach.guilherme@gmail.com>
2026-07-14 10:25:18 +0200

    fix: binary name in tarball

--- Formula/nosh.rb
+++ Formula/nosh.rb
@@ -6,10 +6,10 @@ class Nosh < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gosantos/nosh/releases/download/v0.4.1/nosh-aarch64-apple-darwin.tar.gz"
-      sha256 "60ee7d7598b4f66dd692f37e3d884d779923acc667ef26a1a5493f89f9b4f255" # arm
+      sha256 "7fcddc3f3aba4f1ac45b4a5cc972262b441725c43ffe9c7fbb645f0fd6452968" # arm
     else
       url "https://github.com/gosantos/nosh/releases/download/v0.4.1/nosh-x86_64-apple-darwin.tar.gz"
-      sha256 "c816c5b6475488fc6edb6be0dde33dae0748618aa0b4f468889aa5612ccccb28" # intel
+      sha256 "0d9b115fb82f46e9e5bfa044d80a08ebc83780694aedd61bb92a05195016129d" # intel
     end
   end
 
